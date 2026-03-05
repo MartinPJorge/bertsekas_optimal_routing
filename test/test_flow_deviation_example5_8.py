0b16d6f34a7254a56ae3a7e2c6a9b0f4a5130efb
Jorge Martín Pérez <jorge.martin.perez@upm.es>
2026-03-05 17:49:52 +0100

    Test flow deviation with example 5.8

--- test/test_flow_deviation_example5_8.py
+++ test/test_flow_deviation_example5_8.py
@@ -37,31 +37,32 @@ od_req = {0: 1}
 # x = (x1,x2,x3)
 costs = {
     (0,1): lambda x: 0,
-    (1,4): lambda x: x[0]**2/2,
+    (1,4): lambda x: x**2/2,
     (0,2): lambda x: 0,
-    (2,4): lambda x: x[1]**2/2,
+    (2,4): lambda x: x**2/2,
     (0,3): lambda x: 0,
-    (3,4): lambda x: x[2]**2/2 + .55*x[2],
+    (3,4): lambda x: x**2/2 + .55*x,
 }
 
 # Specify the link cost derivatives
 # x = (x1,x2,x3)
 costs_ = {
     (0,1): lambda x: 0,
-    (1,4): lambda x: x[0],
+    (1,4): lambda x: x,
     (0,2): lambda x: 0,
-    (2,4): lambda x: x[1],
+    (2,4): lambda x: x,
     (0,3): lambda x: 0,
-    (3,4): lambda x: x[2] + .55,
+    (3,4): lambda x: x + .55,
 }
 
 # Specify initial solution (all going down)
-x0 = [0,0,1]
+x0 = {0: [0,0,1]}
 
 # Create the step size function
 alpha_fn = lambda xp,x_: -1\
-    * (xp[0]*(x_[0]-xp[0])+xp[1]*(x_[1]-xp[1])+(.1*x[2]+.55)*(x_[2]-xp[2]))\
-    / ((x_[0]-xp[0])**2+(x_[1]-xp[1])**2+.1*(x_[2]-xp[2])**2)
+    * (xp[0][0]*(x_[0][0]-xp[0][0])+xp[0][1]*(x_[0][1]-xp[0][1])+(.1*xp[0][2]+.55)*(x_[0][2]-xp[0][2]))\
+    / ((x_[0][0]-xp[0][0])**2+(x_[0][1]-xp[0][1])**2+.1*(x_[0][2]-xp[0][2])**2)
 
-flow_deviation(G, od_paths, od_req, costs, costs_, x0, tol=.1,
+flow_deviation(G, od_paths, od_req, costs, costs_, x0, tol=1e-8,
                       alpha_fn=alpha_fn, alpha_gran=100)
+
